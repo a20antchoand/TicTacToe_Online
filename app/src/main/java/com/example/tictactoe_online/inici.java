7b96d411398352630da0fe47052cb334bcbf705b
Marc Langa <a17marlandom@inspedralbes.cat>
2022-04-28 23:38:14 +0200

    inici (gui)

---
+++ app/src/main/java/com/example/tictactoe_online/inici.java
@@ -0,0 +1,20 @@
+package com.example.tictactoe_online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class inici extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inici);
+    }
+
+    public void onClickJugar(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
